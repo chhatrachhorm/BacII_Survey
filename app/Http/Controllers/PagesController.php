5b2a925f567c9eef366f63f99f594b4780f86822
chhatrachhorm <chhatra.kit@gmail.com>
2017-05-26 23:56:34 +0700

    Add nav

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -9,4 +9,13 @@ class PagesController extends Controller
     public function welcome(){
     	return view("welcome");
     }
+    public function survey(){
+    	return view("skillsurvey.survey");
+    }
+    public function contact(){
+    	return view("skillsurvey.contact");
+    }
+    public function about(){
+    	return view("skillsurvey.about");
+    }
 }
---
+++ resources/views/layout/nav.blade.php
@@ -0,0 +1,18 @@
+<nav>
+  <div class="nav-wrapper">
+    <a href="#!" class="brand-logo">SkillPicker</a>
+    <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
+    <ul class="right hide-on-med-and-down">
+      <li><a href="welcome">Welcome</a></li>
+      <li><a href="survey">Skill Picker</a></li>
+      <li><a href="contact">Contact</a></li>
+      <li><a href="about">About</a></li>
+    </ul>
+    <ul class="side-nav" id="mobile-demo">
+      <li><a href="welcome">Welcome</a></li>
+      <li><a href="survey">Skill Picker</a></li>
+      <li><a href="contact">Contact</a></li>
+      <li><a href="about">About</a></li>
+    </ul>
+  </div>
+</nav>
