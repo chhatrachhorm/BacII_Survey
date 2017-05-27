c5ca1773c297130a3592fe33e234eadffcd4260b
chhatrachhorm <chhatra.kit@gmail.com>
2017-05-28 00:18:34 +0700

    Animatiom

--- resources/views/layout/nav.blade.php
+++ resources/views/layout/nav.blade.php
@@ -2,12 +2,12 @@
 <nav>
   <div class="nav-wrapper #0d47a1 blue darken-3 wrapcontent">
     <a href="#!" class="brand-logo">SkillPicker</a>
-    <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
+  <a href="#" data-activates="slide-out" class="button-collapse"><i class="material-icons">menu</i></a>
     <ul class="right hide-on-med-and-down">
-      <li><a href="welcome">Welcome</a></li>
-      <li><a href="survey">Skill Picker</a></li>
-      <li><a href="contact">Contacts</a></li>
-      <li><a href="about"><i class="material-icons">info_outline</i></a></li>
+      <li><a class="waves-effect" href="welcome">Welcome</a></li>
+      <li><a class="waves-effect" href="survey">Skill Picking</a></li>
+      <li><a class="waves-effect" href="contact">Contacts</a></li>
+      <li><a class="waves-effect" href="about"><i class="material-icons">info_outline</i></a></li>
     </ul>
     <ul class="side-nav" id="mobile-demo">
       <li><a href="welcome">Welcome</a></li>
@@ -19,15 +19,15 @@
         <li>
           <div class="userView">
           <div class="background">
-            <img src="images/office.jpg">
+            <img src="images/TumblurBG.jpg">
           </div>
-          <a href="#!user"><img class="circle" src="images/yuna.jpg"></a>
+          <a href="#!user"><img class="circle" src="images/TumblurBG.jpg"></a>
           <a href="#!name"><span class="white-text name">SkillPicker</span></a>
           <a href="#!email"><span class="white-text email">skillpciker@google.org</span></a>
           </div>
         </li>
         <li><a class="waves-effect" href="#!">Welcome</a></li>
-        <li><a class="waves-effect" href="#!">Self-Survey</a></li>
+        <li><a class="waves-effect" href="#!">Skill Picking</a></li>
         <li><div class="divider"></div></li>
         <li><a class="subheader">More</a></li>
         <li><a class="waves-effect" href="#!">Contact</a></li>
@@ -36,11 +36,3 @@
   </div>
 </nav>
 </div>
-
-  <a href="#" data-activates="slide-out" class="button-collapse"><i class="material-icons">menu</i></a>
-  <!-- <ul class="side-nav" id="mobile-demo">
-    <li><a href="welcome">Welcome</a></li>
-    <li><a href="survey">Skill Picker</a></li>
-    <li><a href="contact">Contact</a></li>
-    <li><a href="about">About</a></li>
-  </ul> -->
