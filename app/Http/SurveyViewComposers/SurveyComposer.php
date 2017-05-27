a9ddbf20e60950e29f982f75b22b7992282aaddc
NiobeCipher <jettcalder@gmail.com>
2017-05-27 21:31:24 +0700

    fix fetch data

--- app/Http/SurveyViewComposers/SurveyComposer.php
+++ app/Http/SurveyViewComposers/SurveyComposer.php
@@ -4,10 +4,12 @@ use Illuminate\View\View;
 use DB;
 class SurveyComposer{
   public function compose(View $view){
-    for($id=1; $id<=16; $id++){
-      $answers[$id]=DB::select('select * from answers where q_id = :id', ['id'=>$id]);
-      $questions[$id]=DB::select('select * from questions where q_id = :id', ['id'=>$id]);
+    for($id=0; $id<16; $id++){
+      $answers[$id]=DB::select('select * from answers where q_id = :id', ['id'=>($id+1)]);
+      $questions[$id]=DB::select('select * from questions where q_id = :id', ['id'=>($id+1)]);
     }
+    // print_r($answers[0]);
+    // print_r($questions[0]);
     //$answers = DB::select('select * from answers where q_id = :id', ['id'=>1]);
     $view->with('answers', $answers);
 
