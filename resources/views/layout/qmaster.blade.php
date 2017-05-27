4a71902986425544fd2dc1db8c0a2624673d3a27
NiobeCipher <jettcalder@gmail.com>
2017-05-27 14:06:09 +0700

    fix conflicts

--- resources/views/layout/qmaster.blade.php
+++ resources/views/layout/qmaster.blade.php
@@ -8,25 +8,16 @@
     <link type="text/css" rel="stylesheet" href="css/css/qmaster.css">
     <!--Let browser know website is optimized for mobile-->
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-<<<<<<< HEAD
     <meta name="description" content="@yield('title')">
-=======
-
     <title>@yield('title')</title>
->>>>>>> e4b9929da2579722b4445bf25a95c4b34c75a63b
   </head>
   <body>
-<<<<<<< HEAD
     @include('layout.nav')
     <div class="container">
       <div class="row">
         @yield('survey')
       </div>
     </div>
-=======
-
-    @include('layout.nav')
->>>>>>> e4b9929da2579722b4445bf25a95c4b34c75a63b
     @include('layout.footer')
     <!--Import jQuery before materialize.js-->
     <!--<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>-->
